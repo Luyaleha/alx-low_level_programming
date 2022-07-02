ff7a907f8055b3e0e758a56775ef932648ed1acd
Luyaleha <Luyaleha@gmail.com>
2022-07-02 01:56:14 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,24 +6,9 @@
 */
 int main(void)
 {
-int i, j;
-for (i = '0'; i <= '9'; i++)
-{
-for (j = '0'; j <= '9'; j++)
-{
-if ((i < j) & (j <= '9'))
-{
-putchar(i);
-putchar(j);
-if ((j < '9') | (i < '8'))
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
 putchar('\n');
 return (0);
 }
-
