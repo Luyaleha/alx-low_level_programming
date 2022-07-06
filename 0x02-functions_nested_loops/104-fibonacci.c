ed745433d0cbb31591e5706f6299b491ae213a9b
Luyaleha <Luyaleha@gmail.com>
2022-07-06 08:45:26 -0700

    task16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,12 +10,13 @@ int main(void)
 unsigned long int i, j, k, j1, j2, k1, k2;
 j = 1;
 k = 2;
-printf("%lu", j);
-for (i = 1; i < 91; i++)
+printf("%lu", e);
+for (i = 1; i < 91; ii++)
 {
 printf(", %lu", k);
 k = k + j;
 j = k - j;
+}
 j1 = j / 1000000000;
 j2 = j % 1000000000;
 k1 = k / 1000000000;
@@ -28,8 +29,8 @@ k1 = k1 + j1;
 j1 = k1 - j1;
 k2 = k2 + j2;
 j2 = k2 - j2;
+}
 printf("\n");
 return (0);
 }
-
 
