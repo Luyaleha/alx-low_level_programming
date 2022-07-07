453643a670befdbb2b18cef8a8cea3a3cd5bea06
Luyaleha <Luyaleha@gmail.com>
2022-07-07 14:43:00 -0700

    task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,13 +4,9 @@
 * @n: number of times _ should be printed
 */
 void print_line(int n)
-int a;
-if (n > 0)
-{
-for (a = 0; a < n; a++)
-{
+int count = n, i;
+for (i = 0; i < count; i++)
 _putchar('_');
-}
 }
 _putchar('\n');
 }
