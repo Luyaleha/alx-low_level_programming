eb18cfc599a0a70d8dbb7945f9bed0aa279150c4
Luyaleha <Luyaleha@gmail.com>
2022-07-19 06:50:48 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
-* _memset - Fill memory with a constant byte
-* @s: memory area to fill
-* @b: constant byte to fill
-* @n: bytes of memory area to fill
-* Return: the memory area filled
-*/
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int a;
-for (a = 0; a < n; a++)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
-s[a] = b;
+s[i] = b;
 }
 return (s);
 }
