f5f61239d7bf7e784aac4a24db1618272d65a2ed
Luyaleha <Luyaleha@gmail.com>
2022-08-17 11:44:04 -0700

    taskadva

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,38 +9,27 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new, *temp, *ptr;
-unsigned int i;
-temp = *head;
+unsigned int count = 0;
+listint_t *new_node, *temp = *head;
 if (head == NULL)
 return (NULL);
-new = malloc(sizeof(listint_t));
-if (new == NULL)
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
 return (NULL);
-new->n = n;
-new->next = NULL;
-if (*head == NULL)
-{
-*head = new;
-return (new);
-}
+new_node->n = n;
 if (idx == 0)
-*head = new;
-for (i = 0; i < idx - 1 && temp != NULL && idx != 0; i++)
 {
+new_node->next = temp;
+*head = new_node;
+return (new_node);
+}
+for (; count < (idx - 1); count++)
+{
+if (temp == NULL || temp->next == NULL)
+return (NULL);
 temp = temp->next;
 }
-if (temp == NULL)
-{
-return (NULL);
-}
-if (idx == 0)
-new->next = temp;
-else
-{
-ptr = temp->next;
-temp->next = new;
-new->next = ptr;
-}
-return (new);
+new_node->next = temp->next;
+temp->next = new_node;
+return (new_node);
 }
